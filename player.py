5a7d9402818669f6b354f7e9a3ce8da67024110d
Gabriel Dias <gabrielhdias148@gmail.com>
2021-07-30 01:58:54 -0300

    Refactor player.py code - no significant changes

--- player.py
+++ player.py
@@ -6,43 +6,41 @@ import util
 
 import cursor
 
-current_audio = ''
-buttons = (' ' * 21) + '⏮  ⏸  ⏭'
-volume = 'Vol.: 80%'
-
-volume_db = 0.8
+# player strings that are displayed dynamically in screen
+CURRENT_AUDIO = ''
+BUTTONS = (' ' * 21) + '⏮  ⏸  ⏭'
+VOLUME = 'Vol.: 80%'
+# volume starts at 80%
+VOLUME_DB = 0.8
 
 def draw_player():
-    global current_audio
+    global CURRENT_AUDIO, AUDIO_NAME
 
     util.clear_console()
 
-    if len(current_audio) > 19:
-        current_audio = current_audio[:19] + '...'
-
-    title = (' ' * 18) + 'AUDIO PLAYER'
-    audio_name = (' ' * 2) + f'Playing: {current_audio}'
     bar = '━' * 50
+    title = (' ' * 18) + 'AUDIO PLAYER'
+    if len(CURRENT_AUDIO) > 19:
+        CURRENT_AUDIO = CURRENT_AUDIO[:19] + '...'
+    audio_name = (' ' * 2) + f'Playing: {CURRENT_AUDIO}'
 
-    print(f'\n\n{title}\n\n\n{audio_name} | {volume}\n{bar}\n{buttons}')
-    cursor.hide()
+    print(f'\n\n{title}\n\n\n{audio_name} | {VOLUME}\n{bar}\n{BUTTONS}')
 
 # change the play/pause buttons and play/pause the audio
 def play_pause():
-    global buttons
+    global BUTTONS
     
-    if '⏸' in buttons:
-        buttons = (' ' * 21) + '⏮  ⯈  ⏭'
+    if '⏸' in BUTTONS:
+        BUTTONS = (' ' * 21) + '⏮  ⯈  ⏭'
     else:
-        buttons = (' ' * 21) + '⏮  ⏸  ⏭'
+        BUTTONS = (' ' * 21) + '⏮  ⏸  ⏭'
     
     audio.play_pause_audio()
     draw_player()
 
 # change the volume in 10 steps
 def change_volume(vol):
-    global volume
-    global volume_db
+    global VOLUME, VOLUME_DB
 
     volume_dict = {
         "100%": 1,
@@ -57,26 +55,28 @@ def change_volume(vol):
         "10%": 0.1,
         "0%": 0
     }
+
     if vol <= 1 and vol >= 0:
         for key, value in volume_dict.items():
             if round(vol,1) == value:
-                volume = "Vol.: " + key
-                volume_db = vol
+                VOLUME = "Vol.: " + key
+                VOLUME_DB = vol
                 audio.set_audio_volume(vol)
     draw_player()
 
 
 def start_player(audio_name):
-    global current_audio
-    current_audio = audio_name
+    global CURRENT_AUDIO
+    CURRENT_AUDIO = audio_name
 
     draw_player()
+    cursor.hide()
 
     # listen for keys
     with keyboard.GlobalHotKeys({
             '<ctrl>+<space>': play_pause,
-            '<ctrl>+<up>': lambda: change_volume(vol=volume_db+0.1),
-            '<ctrl>+<down>': lambda: change_volume(vol=volume_db-0.1)}) as listener:
+            '<ctrl>+<up>': lambda: change_volume(vol=VOLUME_DB+0.1),
+            '<ctrl>+<down>': lambda: change_volume(vol=VOLUME_DB-0.1)}) as listener:
         listener.join()
 
 
